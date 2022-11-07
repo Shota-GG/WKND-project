28036d20f257a4b03685fe5820afed4287ade1be
01namele55 <sgogolashvili@gmail.com>
2022-11-07 23:29:40 +0400

    firstsection2

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 let account = document.querySelector(".acc")
 let signup = document.querySelector(".signup")
+let sign = document.querySelector(".sign")
+let search = document.querySelector(".search")
 
 account.addEventListener("click", function(){
-    signup.style.display = "block"
+        signup.style.display = "block"
 })
+sign.addEventListener("click", function(){
+        signup.style.display = "none"
+})
+search.onclick = function(){
+    document.querySelector(".search-box").classList.toggle("active")
+}
